ba3ee5b0732fcff6deb18a65a1587c7e54ca1ee2
Marco Bonifacio <bonifacio.marco@gmail.com>
2023-12-29 00:33:18 +0100

    transformation

--- static_monte_carlo.py
+++ static_monte_carlo.py
@@ -1,7 +1,16 @@
 from datetime import datetime
 from typing import Generator
 
-def uniform(
+import numpy as np
+
+# Uniform random variable generator
+# Steps:
+# 1, Fix integers modulus, multiplier, increment
+# 2. Set up a seed between 0 and m - 1
+# 3. Run the recursive rule x1 = (a * x + c) % m
+# 4. Yield x1 / m beetween 0 and 1
+
+def uniform_sampling(
 	x0: int = datetime.now().microsecond, a: int = 40_692,
 	m: int = 2_147_483_399, c: int = 0
 ) -> Generator[float, None, None]:
@@ -21,6 +30,11 @@ def uniform(
 		x0 = modulus(x0)
 		yield x0 / m
 
+# Stratified sampling techique
+# Steps:
+# 1. Fix integers number of bins and number of samples batches
+# 2. Cycle over batches and bins, scaling a uniform random variable for bin
+
 def stratified_sampling(
 	M: int, k: int
 ) -> Generator[float, None, None]:
@@ -29,5 +43,21 @@ def stratified_sampling(
 	M: number of stratifying bins
 	k: number of samples batches
 	"""
-	u = uniform()
+	u = uniform_sampling()
 	return ((next(u) + i) / M for j in range(k) for i in range(M))
+
+# Transformation methods: inverse transformation
+# Steps:
+# 1. Simulate a uniform random variable
+# 2. Apply tje inverse function
+
+def exponential_sampling(
+	lambda_: float = 1
+) -> Generator[float, None, None]:
+	"""
+	Draw a pseudo-random sample from an exponential distribution.
+	lambda_: parameter of exponential distribution (default: 1)
+	"""
+	u = uniform_sampling()
+	while True:
+		yield -(1 / lambda_) * np.log(next(u)) 
